796d91ed3f121e886f2a5b7ed2e9ab47decb2a89
Paul Montgomery <finch.wren.swift@gmail.com>
2024-09-02 18:40:17 -0700

    Markdown is hard

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 # URL 
 https://kanjialive.com/214-traditional-kanji-radicals/
+
+# Run it
+Step 0 : Have flask 
+Step 1 : python main.py
+Step 2 : in browser go to localhost:5000 
+Step 3 : Press buttons. It is drag aware. You can shrink or enlarge the radicals
+
+## Images
+
+### Wind Goes Slowly
+![Wind Goes Slowly](wind_go_slowly.png)
+
+### Person Period Month
+![Person Period Month](person_period_month.png)
+
+### Wind Horn
+![Wind Horn](wind_horn.png)
--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-
-from flask import Flask, redirect, url_for, request, render_template
-from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
-import sqlite3
-import json
-from flask import jsonify
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route('/main', methods=['GET', 'POST'])
-def main1():
-    print("main")
-    return render_template('index.html')
-    #return render_template('index_is_logged_in.html', stores=stores, vendingMachines=vendingMachines, inventory=inventory )
 
 @app.route('/')
-def main2():
-    print("index")
+def index():
     return render_template('index.html')
 
-
-
 if __name__ == '__main__':
-    app.debug = True # REMOVE THIS ONCE IN PRODUCTION
-    print("http://34.82.219.228:8080 with database at data/kanji_db.db")
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    print("http://localhost:5000/")
+    app.run(debug=True)
